3b1e23f4f18a73ce9da1a3ebbfab207b7bb8844e
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2025-10-15 11:28:54 -0500

    subo las 3 primeras clases

---
+++ Lab_ClasesYReglas_Poo/Lab_ClasesYReglas_Poo/Calificacion .cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_ClasesYReglas_Poo
+{
+    internal class Calificacion
+    {
+        private Estudiante estudiante;
+        private Materia materia;
+        private double nota;
+
+        public Estudiante Estudiante
+        {
+            get { return estudiante; }
+            set { estudiante = value; }
+        }
+
+        public Materia Materia
+        {
+            get { return materia; }
+            set { materia = value; }
+        }
+
+        public double Nota
+        {
+            get { return nota; }
+            set
+            {
+                if (value < 0)
+                    nota = 0;
+                else if (value > 100)
+                    nota = 100;
+                else
+                    nota = value;
+            }
+        }
+
+        public Calificacion(Estudiante estudiante, Materia materia, double nota)
+        {
+            Estudiante = estudiante;
+            Materia = materia;
+            Nota = nota;
+        }
+
+        public double CalcularPuntos()
+        {
+            return Nota * Materia.Creditos;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("=== Datos de la Calificación ===");
+            Console.WriteLine($"Estudiante: {Estudiante.Nombre}");
+            Console.WriteLine($"Materia: {Materia.Nombre}");
+            Console.WriteLine($"Nota: {Nota}");
+        }
+    }
+}
---
+++ Lab_ClasesYReglas_Poo/Lab_ClasesYReglas_Poo/Materia.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_ClasesYReglas_Poo
+{
+    internal class Materia
+    {
+        private string nombre;
+        private string codigo;
+        private int creditos;
+        private int cupos;
+        private int inscritos;
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public int Creditos
+        {
+            get { return creditos; }
+            set
+            {
+                if (value > 0)
+                    creditos = value;
+                else
+                    creditos = 1; 
+            }
+        }
+
+        public int Cupos
+        {
+            get { return cupos; }
+            set
+            {
+                if (value >= 0)
+                    cupos = value;
+                else
+                    cupos = 0;
+            }
+        }
+
+        public int Inscritos
+        {
+            get { return inscritos; }
+            set
+            {
+                if (value >= 0 && value <= cupos)
+                    inscritos = value;
+                else if (value < 0)
+                    inscritos = 0;
+                else
+                    inscritos = cupos; 
+            }
+        }
+
+        public Materia(string nombre, string codigo, int creditos, int cupos = 0, int inscritos = 0)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Creditos = creditos;
+            Cupos = cupos;
+            Inscritos = inscritos;
+        }
+
+        public Materia()
+        {
+            Nombre = "Sin nombre";
+            Codigo = "N/A";
+            Creditos = 1;
+            Cupos = 0;
+            Inscritos = 0;
+        }
+
+        public int CalcularCargaSemanal(int horasPorCredito)
+        {
+            return Creditos * horasPorCredito;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("=== Datos de la materia ===");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Código: {Codigo}");
+            Console.WriteLine($"Créditos: {Creditos}");
+            Console.WriteLine($"Cupos/Inscritos: {Inscritos}/{Cupos}");
+        }
+    }
+}
